d4dfc3789060ad0fe6d8cdb44b74ddebbbbcb64a
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-10-25 17:33:39 +0300

    Initial Commit

---
+++ BrawHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrawHW
+//
+//  Created by Stanislav Sobolevsky on 25.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
